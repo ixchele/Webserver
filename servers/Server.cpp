089274851ee8fb81111d69781d71208e853501dd
Ayman Abourizq <aabouriz@c4r6p9.1337.ma>
2026-05-09 19:29:37 +0100

    editing Server class

--- servers/Server.cpp
+++ servers/Server.cpp
@@ -1,7 +1,28 @@
 #include "Server.hpp"
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 void Server::run()
 {
-	creat_socket();
+    creat_socket();
+    bind_address();
+    start_listening();
+}
 
+void Server::creat_socket()
+{
+	this->m_sockFd = socket(AF_INET, SOCK_STREAM, 0);
+}
+
+void Server::bind_address()
+{
+	bind(m_sockFd, reinterpret_cast<sockaddr*>(&this->m_sockaddr), 16);
+}
+
+void Server::start_listening()
+{
+}
+
+void Server::accept_client()
+{
 }
--- servers/Server.hpp
+++ servers/Server.hpp
@@ -1,7 +1,8 @@
 #pragma once
-#include <sys/socket.h>
 #include <string>
+#include <sys/socket.h>
 #include <vector>
+#include <netinet/in.h>
 
 using std::string;
 using std::vector;
@@ -14,28 +15,32 @@ using std::vector;
 #define M_POST 2
 #define M_DELETE 4
 
-typedef  char	methods_t;
+typedef char methods_t;
 
-class Server {
-	public:
-		int get_sockFd();
-		void run();
+class Server
+{
+  public:
+    int get_sockFd();
+    void creat_socket();
+    void bind_address();
+    void start_listening();
+    void accept_client();
+    void run();
 
+  private:
+    struct Location
+    {
+        string root;
+        methods_t methods;
+        string index;
+        bool autoindex;
+    };
 
-	private:
-		struct	Location {
-			string		root;
-			methods_t	methods;
-			string		index;
-			bool		autoindex;
-		};
-
-		int 				m_sockFd;
-		short 				m_port; // NOTE : same as listen
-		int					m_address; // NOTE : same as host
-		string				m_serverName;
-		int					m_uploadLimit;
-		vector<Location>	m_locations;
-		
-
+    int m_sockFd;
+    short m_port;  // NOTE : same as listen
+    int m_address; // NOTE : same as host
+		struct sockaddr_in m_sockaddr;
+    string m_serverName;
+    int m_uploadLimit;
+    vector <Location> m_locations;
 };
