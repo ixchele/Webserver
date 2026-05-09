6fa7e12a78154106b913c13d015b99759e715c65
Ayman Abourizq <aabouriz@c3r1p5.1337.ma>
2026-05-09 15:30:51 +0100

    editing Server class

--- servers/Server.hpp
+++ servers/Server.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <sys/socket.h>
+#include <string>
+using namespace std;
 
 // each server in config file will be inistiated from this class
 class Server {
@@ -9,5 +11,8 @@ public:
 
 
 private:
-	int m_sockFd;
+	int 		m_sockFd;
+	short 	m_port;
+	int			m_address;
+	string	m_name;
 };
