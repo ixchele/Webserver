ccf91ed8a643c0166cc4d725417c9ad380d1889b
Zakaria Bengueddime <zbengued@e1r8p12.1337.ma>
2026-05-09 17:37:21 +0100

    serverConfig proto

--- servers/Server.hpp
+++ servers/Server.hpp
@@ -1,18 +1,41 @@
 #pragma once
 #include <sys/socket.h>
 #include <string>
-using namespace std;
+#include <vector>
+
+using std::string;
+using std::vector;
 
 // each server in config file will be inistiated from this class
+//
+//
+
+#define M_GET 1
+#define M_POST 2
+#define M_DELETE 4
+
+typedef  char	methods_t;
+
 class Server {
-public:
-	int get_sockFd();
-	void run();
+	public:
+		int get_sockFd();
+		void run();
 
 
-private:
-	int 		m_sockFd;
-	short 	m_port;
-	int			m_address;
-	string	m_name;
+	private:
+		struct	Location {
+			string		root;
+			methods_t	methods;
+			string		index;
+			bool		autoindex;
+		};
+
+		int 				m_sockFd;
+		short 				m_port; // NOTE : same as listen
+		int					m_address; // NOTE : same as host
+		string				m_serverName;
+		int					m_uploadLimit;
+		vector<Location>	m_locations;
+		
+
 };
