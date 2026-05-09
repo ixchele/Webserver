c10e7904027230123fb4f7909709496a9b6bf24b
Zakaria Bengueddime <zbengued@e1r8p12.1337.ma>
2026-05-09 16:53:26 +0100

    refactor tokenizer

--- ConfigFileParser/ConfigFileParser.hpp
+++ ConfigFileParser/ConfigFileParser.hpp
@@ -1,13 +1,32 @@
 #pragma once
 
+//  NOTE : BNF
+//
+// <config>            ::= <server_block> { <server_block> }
+//
+// <server_block>      ::= "server" "{" { <server_directive> } "}"
+//
+// <server_directive>  ::= <listen> | <host> | <server_name> | <body_size>
+// 						   | <error_page> | <location_block>
+//
+// <location_block>    ::= "location" <path> "{" { <location_directive> } "}"
+//
+// <location_directive> ::= <methods> | <root> | <index> | <autoindex>
+// 						    | <return> | <upload_path> | <cgi_pass>
+//
+// <listen>            ::= "listen" <port_number> ";"
+// <methods>           ::= "methods" <method_name> { <method_name> } ";"
+// <error_page>        ::= "error_page" <error_code> <file_path> ";"
+
+
 #include <string>
 #include <vector>
 #include <sstream>
 
-typedef std::string	Token;
+typedef struct Token Token;
 typedef std::vector<Token> TokenList;
 
-TokenList	toknizer(const std::string &content);
 
+TokenList	toknizer(const std::string &content);
 
 #include <ConfigFileParser.template.hpp>
--- ConfigFileParser/tokenizer.cpp
+++ ConfigFileParser/tokenizer.cpp
@@ -1,8 +1,45 @@
-#include <ConfigFileParser.hpp>
+#include <cstddef>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-TokenList	toknizer(const std::string &content) {
-	std::stringstream			ss(content);
-	std::string					token;
+class	ConfigFile {
+	private:
+		std::string	_confContent;
+
+	public:
+		ConfigFile(const std::string &confPath);
+
+		struct ConfigException : public std::runtime_error {
+			ConfigException(const std::string &error);
+		};
+};
+
+ConfigFile::ConfigFile(const std::string &confPath) {
+	std::ifstream	ifs(confPath.c_str());	
+	if (!ifs.is_open())
+		throw ConfigException("[x] Error : could not open config file \'" + confPath + "'" + ".");
+	
+	ifs >> this->_confContent;
+}
+
+std::string	AereContent(const std::string	&content, const std::string &tokens) {
+	std::string	spaced;
+
+	for (std::size_t i = 0; i < content.size(); ++i) {
+		if (tokens.find(content[i]) != std::string::npos)
+			spaced += " " + std::string(&content[i]) + " ";
+		else
+			spaced += content[i];
+	}
+
+	return spaced;
+}
+
+std::vector<std::string>	toknizer(const std::string &content) {
+	std::stringstream	ss(AereContent(content, "{};"));
+	std::string			token;
 	std::vector<std::string>	tokenList;
 
 	while (ss >> token)
