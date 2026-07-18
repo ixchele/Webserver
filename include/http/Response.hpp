ac9a6b8e10163089217c45711fd2e0dcf2950731
DGankutsuou <aymanabourizq@gmail.com>
2026-07-18 17:29:04 +0000

    creating the response method

--- include/http/Response.hpp
+++ include/http/Response.hpp
@@ -1,13 +1,19 @@
 #ifndef RESPONSE_HPP
-# define RESPONSE_HPP
-# include <Request.hpp>
-# include <Client.hpp>
-# include <fstream>
-# include <string>
+#define RESPONSE_HPP
+#include <HttpStatus.hpp>
+#include <Request.hpp>
+#include <Client.hpp>
+#include <fstream>
+#include <string>
 
 class HttpResponse
 {
   public:
+    enum State {
+        Building,
+        Sending
+    };
+
     HttpResponse(Client *client, HttpRequest *request);
     ~HttpResponse();
 
@@ -16,9 +22,13 @@ class HttpResponse
   private:
     const Client *_client;
     HttpRequest *_request;
-    std::ifstream _bodyFile;
+    State _state;
+    int _bodyFile;
 
-    std::string _get_error_message(int code);
+    std::string _get_code_message(HttpStatus::Code code);
+    // void _get();
+    // void _post();
+    void _delete();
 };
 
 #endif
--- src/http/Response.cpp
+++ src/http/Response.cpp
@@ -1,4 +1,9 @@
+#include <HttpStatus.hpp>
 #include <Response.hpp>
+#include <iostream>
+#include <unistd.h>
+#include <errno.h>
+#include <string>
 
 
 HttpResponse::HttpResponse(Client *client, HttpRequest *request)
@@ -10,19 +15,90 @@ HttpResponse::~HttpResponse()
 {
 }
 
-void HttpResponse::response() {
-    
+void HttpResponse::_delete() {
+    // To do: I must check the config
+
+    if (unlink(_request->getUri().c_str()) == -1)
+    {
+        switch (errno)
+        {
+            case ENOENT: 
+        }
+    }
 }
 
-std::string HttpResponse::_get_error_message(int code) {
+void HttpResponse::response() {
+    if (_request == NULL || _client == NULL)
+    {
+        std::cerr << "warning: response couldn't find client or request" << std::endl;
+        return ;
+    }
+    if (_state == Building)
+    {
+        switch (_request->getMethod())
+        {
+            case HttpRequest::DELETE: _delete(); break; 
+            // case HttpRequest::GET: _get(); break; 
+            // case HttpRequest::POST: _post(); break; 
+        }
+    }
+}
+
+std::string HttpResponse::_get_code_message(HttpStatus::Code code) {
     switch(code) {
-        case 400: return "Bad Request";
-        case 403: return "Forbidden";
-        case 404: return "Not Found";
-        case 405: return "Method Not Allowed";
-        case 413: return "Payload Too Large";
-        case 500: return "Internal Server Error";
-        case 501: return "Not Implemented";
-        default:  return "Error";
+        // 1xx: Informational
+        case HttpStatus::Continue:                    return "Continue";
+        case HttpStatus::SwitchingProtocols:          return "Switching Protocols";
+
+        // 2xx: Successful
+        case HttpStatus::OK:                          return "OK";
+        case HttpStatus::Created:                     return "Created";
+        case HttpStatus::Accepted:                    return "Accepted";
+        case HttpStatus::NonAuthoritativeInformation: return "Non-Authoritative Information";
+        case HttpStatus::NoContent:                   return "No Content";
+        case HttpStatus::ResetContent:                return "Reset Content";
+        case HttpStatus::PartialContent:              return "Partial Content";
+
+        // 3xx: Redirection
+        case HttpStatus::MultipleChoices:             return "Multiple Choices";
+        case HttpStatus::MovedPermanently:            return "Moved Permanently";
+        case HttpStatus::Found:                       return "Found";
+        case HttpStatus::SeeOther:                    return "See Other";
+        case HttpStatus::NotModified:                 return "Not Modified";
+        case HttpStatus::UseProxy:                    return "Use Proxy";
+        case HttpStatus::TemporaryRedirect:           return "Temporary Redirect";
+        case HttpStatus::PermanentRedirect:           return "Permanent Redirect";
+
+        // 4xx: Client Error
+        case HttpStatus::BadRequest:                  return "Bad Request";
+        case HttpStatus::Unauthorized:                return "Unauthorized";
+        case HttpStatus::PaymentRequired:             return "Payment Required";
+        case HttpStatus::Forbidden:                   return "Forbidden";
+        case HttpStatus::NotFound:                    return "Not Found";
+        case HttpStatus::MethodNotAllowed:            return "Method Not Allowed";
+        case HttpStatus::NotAcceptable:               return "Not Acceptable";
+        case HttpStatus::ProxyAuthenticationRequired: return "Proxy Authentication Required";
+        case HttpStatus::RequestTimeout:              return "Request Timeout";
+        case HttpStatus::Conflict:                    return "Conflict";
+        case HttpStatus::Gone:                        return "Gone";
+        case HttpStatus::LengthRequired:              return "Length Required";
+        case HttpStatus::PreconditionFailed:          return "Precondition Failed";
+        case HttpStatus::PayloadTooLarge:             return "Payload Too Large";
+        case HttpStatus::URITooLong:                  return "URI Too Long";
+        case HttpStatus::UnsupportedMediaType:        return "Unsupported Media Type";
+        case HttpStatus::RangeNotSatisfiable:         return "Range Not Satisfiable";
+        case HttpStatus::ExpectationFailed:           return "Expectation Failed";
+        case HttpStatus::UpgradeRequired:             return "Upgrade Required";
+
+        // 5xx: Server Error
+        case HttpStatus::InternalServerError:         return "Internal Server Error";
+        case HttpStatus::NotImplemented:              return "Not Implemented";
+        case HttpStatus::BadGateway:                  return "Bad Gateway";
+        case HttpStatus::ServiceUnavailable:          return "Service Unavailable";
+        case HttpStatus::GatewayTimeout:              return "Gateway Timeout";
+        case HttpStatus::HTTPVersionNotSupported:     return "HTTP Version Not Supported";
+
+        // Fallback
+        default:                                      return "Unknown Error";
     }
 }
